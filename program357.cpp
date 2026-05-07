f339ce70cc416c261634ce00fa8ef0244254f9a5
Gajanan Sunil Dimble <gajanandimble2355@gmail.com>
2026-05-07 22:12:49 +0530

    Add files via upload

---
+++ program357.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int Addition(int A,int B)
+{
+    return A+B;
+}
+int main()
+{
+    int Ret=0;
+    int Ret=Addition(10,11);
+    cout<<"Addition is:"<<Ret<<"\n";
+    return 0;
+}
